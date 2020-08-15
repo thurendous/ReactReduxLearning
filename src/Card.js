d3e9ed27d8f6bb446227af14aa5790731043baa0
thurendous <thurendous2@gmail.com>
2020-08-16 02:46:55 +0900

    8/16 daily grind

---
+++ src/Card.js
@@ -0,0 +1,24 @@
+import React from "react";
+// import this in order to use JSX
+
+
+
+const Card = ({ id, name, email }) => {
+
+    return (
+        <div className="tc bg-light-green dib br-pill pa3 ma2 grow shadow-5 fl w-24 b--dashed bw1 b--hot-pink dim">
+            <img src={`https://robohash.org/${name}`} alt="robos" />
+            <div>
+                <h2>
+                    {id}<br />
+                    {name}
+                </h2>
+                <p>
+                    {email}
+                </p>
+            </div>
+        </div >
+    );
+}
+
+export default Card;
---
+++ src/robots.js
@@ -0,0 +1,58 @@
+export const robots = [
+    {
+        id: 1,
+        name: "吴曾桢",
+        username: "satousaori",
+        email: "satousaori@gmail.com"
+    },
+    {
+        id: 2,
+        name: "zengzhenwu",
+        username: "thurndous",
+        email: "thureous@gmail.com"
+    },
+    {
+        id: 3,
+        name: "yangyu",
+        username: "Yangyu",
+        email: "thureus4@gmail.com"
+
+    },
+    {
+        id: 5,
+        name: "wengengzhang",
+        username: "wengengzhang",
+        email: "wengengzhang@gmail.com"
+    },
+    {
+        id: 6,
+        name: "于洋",
+        username: "wengengzhang",
+        email: "wengengzhang@gmail.com"
+    },
+    {
+        id: 7,
+        name: "呉曾テイ",
+        username: "wengengzhang",
+        email: "wengengzhang@gmail.com"
+    },
+    {
+        id: 8,
+        name: "張文コン",
+        username: "wengengzhang",
+        email: "wengengzhang@gmail.com"
+    },
+    {
+        id: 8,
+        name: "于洋",
+        username: "wengengzhang",
+        email: "wengengzhang@gmail.com"
+    },
+    {
+        id: 8,
+        name: "张文庚",
+        username: "wengengzhang",
+        email: "wengengzhang@gmail.com"
+    },
+
+]
